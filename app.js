441cf1cdcfc47ab58e31ee0cdf8deb1d3475b562
Rohit15504 <rohitrana7457@gmail.com>
2025-02-28 15:25:30 +0530

    modified root path

--- app.js
+++ app.js
@@ -71,6 +71,10 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
+
 //Wrong Route
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
